8228e557d3816af6a92685935bbf18c32e3d0a6d
= <=>
2021-09-21 15:11:53 +0700

    complete index - fe

---
+++ js/slider.js
@@ -0,0 +1,40 @@
+var swiper = new Swiper(".products-slide", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay:{
+        delay:3500,
+        disableOnInteraction:false,
+    },
+    centeredSlides:true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+  });
+  var swiper = new Swiper(".review-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay:{
+        delay:3500,
+        disableOnInteraction:false,
+    },
+    centeredSlides:true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+  });
